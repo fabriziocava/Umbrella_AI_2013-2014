5600c30bbb99b273ee98707fbd640ca210a6d315
fabriziocava <fabrizio.cava88@gmail.com>
2013-12-05 10:38:47 +0100

    INIT TASK B

---
+++ Umbrella_VacuumCleaner_TASK_2/src/agent/IntelligentMove_TASK_2.java
@@ -0,0 +1,5 @@
+package agent;
+
+public class IntelligentMove_TASK_2 {
+
+}
--- Umbrella_VacuumCleaner_TASK_2/src/agent/VacuumAgent1.java
+++ Umbrella_VacuumCleaner_TASK_2/src/agent/VacuumAgent1.java
@@ -1,9 +1,6 @@
 package agent;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
 
@@ -11,69 +8,27 @@ import aima.core.agent.Action;
 import aima.core.agent.AgentProgram;
 import aima.core.agent.Percept;
 import aima.core.agent.impl.AbstractAgent;
-import aima.core.agent.impl.NoOpAction;
-import aima.core.environment.map.Map;
-import aima.core.util.datastructure.Pair;
 import core.LocalVacuumEnvironmentPercept;
-import core.VacuumEnvironment.LocationState;
+import core.LocalVacuumEnvironmentPerceptTaskEnvironmentB;
 
 public class VacuumAgent1 extends AbstractAgent {
-	
-	IntelligentMove intelligentMove;
-	ArrayList<Integer> listMovements;
-	private boolean firstStep;
-	
+
 	public VacuumAgent1() {
-		firstStep = true;
-		this.program = new AgentProgram() {
+		super(new AgentProgram() {
 			@Override
 			public Action execute(final Percept percept) {
-				final LocalVacuumEnvironmentPercept vep = (LocalVacuumEnvironmentPercept) percept;
+				final LocalVacuumEnvironmentPerceptTaskEnvironmentB vep = (LocalVacuumEnvironmentPerceptTaskEnvironmentB) percept;
 				final Set<Action> actionsKeySet = vep.getActionEnergyCosts().keySet();
-				if(firstStep) {
-					intelligentMove = new IntelligentMove(vep);
-					listMovements = intelligentMove.getListMovements();
-					firstStep=false;
-				}
-				int moveInt=0;
-				while(listMovements.size()!=0 && vep.getCurrentEnergy()>0) {
-					moveInt=listMovements.get(0);
-					final Iterator<Action> iterator = actionsKeySet.iterator();
-					for (int i = 0; i < moveInt; i++) {
-						iterator.next();
-					}
-					listMovements.remove(0);
-					return iterator.next();
-				}
-				System.out.println("ENERGY: " + vep.getCurrentEnergy());
-				return NoOpAction.NO_OP;
+
+
+				new Random();
+				final int randomInt = new Random().nextInt(actionsKeySet.size());
+				final Iterator<Action> iterator = actionsKeySet.iterator();
+				for (int i = 0; i < randomInt; i++)
+					iterator.next();
+				return iterator.next();
 			}
-		};
+		});
 	}
-	
-	
-/*
-	@Override
-	public Action execute(Percept percept) {
-		final LocalVacuumEnvironmentPercept vep = (LocalVacuumEnvironmentPercept) percept;
-		final Set<Action> actionsKeySet = vep.getActionEnergyCosts().keySet();
-		if(firstStep) {
-			intelligentMove = new IntelligentMove(vep);
-			listMovements = intelligentMove.getListMovements();
-			firstStep=false;
-		}
-		int moveInt=0;
-		while(listMovements.size()!=0) {
-			moveInt=listMovements.get(0);
-			final Iterator<Action> iterator = actionsKeySet.iterator();
-			for (int i = 0; i < moveInt; i++) {
-				iterator.next();
-			}
-			listMovements.remove(0);
-			return iterator.next();
-		}
-		return actionsKeySet.iterator().next();
-	}
-*/	
 
 }
