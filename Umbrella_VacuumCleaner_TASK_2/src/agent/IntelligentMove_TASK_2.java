5fffbd834116b0129c3c903eac95565612cd6617
fabriziocava <fabrizio.cava88@gmail.com>
2013-12-05 12:07:18 +0100

    COMMIT

--- Umbrella_VacuumCleaner_TASK_2/src/agent/IntelligentMove_TASK_2.java
+++ Umbrella_VacuumCleaner_TASK_2/src/agent/IntelligentMove_TASK_2.java
@@ -1,5 +1,47 @@
 package agent;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Set;
+
+import aima.core.agent.Action;
+import core.LocalVacuumEnvironmentPerceptTaskEnvironmentB;
+import core.VacuumEnvironment.LocationState;
+
 public class IntelligentMove_TASK_2 {
+	
+	final static int SUCK = 0;
+	final static int LEFT = 1;
+	final static int DOWN = 2;
+	final static int RIGHT = 3;
+	final static int UP = 4;
+	
+	private double THRESHOLD;
+	private boolean isOverThreshold;
+	
+	private LocalVacuumEnvironmentPerceptTaskEnvironmentB vep;
+	private Set<Action> actionsKeySet;
+	
+	public IntelligentMove_TASK_2(LocalVacuumEnvironmentPerceptTaskEnvironmentB vep) {
+		this.vep=vep;
+		THRESHOLD = vep.getInitialEnergy()/2; /*DA CAMBIARE IN BASE ALLA SIZE DELLA MAPPA*/
+		isOverThreshold = false;
+	}
+	
+	public int getMovement() {
+		new Random();
+		int randomInt = new Random().nextInt(5);
+		while(randomInt==SUCK)
+			randomInt = new Random().nextInt(5);
+		int movement = randomInt;
+		if(vep.getCurrentEnergy()<THRESHOLD)
+			isOverThreshold = true;
+		if(!isOverThreshold) {
+			System.out.println(vep.getState().getLocState());
+			if(vep.getState().getLocState()==LocationState.Dirty)
+				movement=SUCK;	
+		}
+		return movement;
+	}
 
 }
--- Umbrella_VacuumCleaner_TASK_2/src/agent/VacuumAgent1.java
+++ Umbrella_VacuumCleaner_TASK_2/src/agent/VacuumAgent1.java
@@ -1,34 +1,39 @@
 package agent;
 
 import java.util.Iterator;
-import java.util.Random;
 import java.util.Set;
 
 import aima.core.agent.Action;
 import aima.core.agent.AgentProgram;
 import aima.core.agent.Percept;
 import aima.core.agent.impl.AbstractAgent;
-import core.LocalVacuumEnvironmentPercept;
 import core.LocalVacuumEnvironmentPerceptTaskEnvironmentB;
 
 public class VacuumAgent1 extends AbstractAgent {
-
+	
+	private LocalVacuumEnvironmentPerceptTaskEnvironmentB vep;
+	private Set<Action> actionsKeySet;
+	private IntelligentMove_TASK_2 intelligentMoveTask2;
+	private boolean firstStep;
+	
 	public VacuumAgent1() {
-		super(new AgentProgram() {
+		firstStep = true;
+		this.program = new AgentProgram() {
 			@Override
 			public Action execute(final Percept percept) {
-				final LocalVacuumEnvironmentPerceptTaskEnvironmentB vep = (LocalVacuumEnvironmentPerceptTaskEnvironmentB) percept;
-				final Set<Action> actionsKeySet = vep.getActionEnergyCosts().keySet();
-
-
-				new Random();
-				final int randomInt = new Random().nextInt(actionsKeySet.size());
+				if(firstStep) {
+					vep = (LocalVacuumEnvironmentPerceptTaskEnvironmentB) percept;
+					actionsKeySet = vep.getActionEnergyCosts().keySet();
+					intelligentMoveTask2 = new IntelligentMove_TASK_2(vep);
+					firstStep = false;
+				}
+				int move = intelligentMoveTask2.getMovement();
 				final Iterator<Action> iterator = actionsKeySet.iterator();
-				for (int i = 0; i < randomInt; i++)
+				for (int i = 0; i < move; i++)
 					iterator.next();
 				return iterator.next();
 			}
-		});
+		};
 	}
 
 }
