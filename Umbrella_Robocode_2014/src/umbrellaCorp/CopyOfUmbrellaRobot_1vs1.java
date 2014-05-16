fbcb06c318366d07930838445e5b28cc1a38de35
fabriziocava <fabrizio.cava88@gmail.com>
2014-05-16 14:57:02 +0200

    update functions

--- Umbrella_Robocode_2014/src/umbrellaCorp/CopyOfUmbrellaRobot_1vs1.java
+++ Umbrella_Robocode_2014/src/umbrellaCorp/CopyOfUmbrellaRobot_1vs1.java
@@ -8,9 +8,14 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 public class CopyOfUmbrellaRobot_1vs1 extends AdvancedRobot {
 	
-	private int moveDirection = 1;
-	private int wallMargin = 60; 
+	private static final double DOUBLE_PI = (Math.PI*2);
+	private static final double HALF_PI = (Math.PI/2);
+	private static final double WALL_AVOID_INTERVAL = 10;
+	private static final double WALL_AVOID_FACTORS = 20;
+	private static final double WALL_AVOID_DISTANCE = (WALL_AVOID_INTERVAL * WALL_AVOID_FACTORS);
 	
+	
+	private int direction = 1;	
 	private double idealDistance;
 	private double enemyDistance = 0.0;
 	private double enemyBearing = 0.0;
@@ -20,13 +25,15 @@ public class CopyOfUmbrellaRobot_1vs1 extends AdvancedRobot {
 				
 		setColors(Color.red,Color.white,Color.white);
 		
-		//goOnBorder();
 //		turnRadarRightRadians(Double.POSITIVE_INFINITY);
 		turnGunRightRadians(Double.POSITIVE_INFINITY);
 		while(true) {
-			scan();
-			//move();
-			doMove();
+            // Always attempt to move straight ahead - the
+            // adjustHeadingForWalls method will handle wall avoidance
+            setTurnRightRadiansOptimal(adjustHeadingForWalls(0));
+            scan();
+            doMove();
+            execute();
 		}
 	}
 	
@@ -123,9 +130,9 @@ public class CopyOfUmbrellaRobot_1vs1 extends AdvancedRobot {
 	
 	public void doMove() {
 		if(getVelocity()==0)
-			moveDirection *= -1;
+			direction *= -1;
 		setTurnRight(enemyBearing+90);
-		setAhead(100*moveDirection);
+		setAhead(100*direction);
 	}
 	
 	/*
@@ -136,6 +143,143 @@ public class CopyOfUmbrellaRobot_1vs1 extends AdvancedRobot {
 	/*
 	 * UTILITY
 	 */
+	
+	private double adjustHeadingForWalls(double heading) {
+        double fieldHeight = getBattleFieldHeight();
+        double fieldWidth = getBattleFieldWidth();
+        double centerX = (fieldWidth / 2);
+        double centerY = (fieldHeight / 2);
+        
+        double currentHeading = getRelativeHeadingRadians();
+        double x = getX();
+        double y = getY();
+        
+        boolean nearWall = false;
+        
+        double desiredX;
+        double desiredY;
+        
+        if ((y < WALL_AVOID_DISTANCE) ||
+                ((fieldHeight - y) < WALL_AVOID_DISTANCE)) {
+            desiredY = centerY;
+            nearWall = true;
+        } else {
+            desiredY = y;
+        }
+        if ((x < WALL_AVOID_DISTANCE) ||
+                ((fieldWidth - x) < WALL_AVOID_DISTANCE)) {
+            desiredX = centerX;
+            nearWall = true;
+        } else {
+            desiredX = x;
+        }
+        if (nearWall) {
+            double desiredBearing = calculateBearingToXYRadians(x,
+                    y,
+                    currentHeading,
+                    desiredX,
+                    desiredY);
+            double distanceToWall = Math.min(
+                    Math.min(x, (fieldWidth - x)),
+                    Math.min(y, (fieldHeight - y)));
+            int wallFactor =
+                    (int)Math.min((distanceToWall / WALL_AVOID_INTERVAL),
+                            WALL_AVOID_FACTORS);
+            return ((((WALL_AVOID_FACTORS - wallFactor) * desiredBearing) +
+                    (wallFactor * heading)) / WALL_AVOID_FACTORS);
+        } else {
+            return heading;
+        }
+	}
+	
+    public double getRelativeHeadingRadians() {
+        double relativeHeading = getHeadingRadians();
+        if (direction < 1) {
+            relativeHeading =
+                    normalizeAbsoluteAngleRadians(relativeHeading + Math.PI);
+        }
+        return relativeHeading;
+    }
+    
+    public void reverseDirection() {
+        double distance = (getDistanceRemaining() * direction);
+        direction *= -1;
+        setAhead(distance);
+    }
+    
+    public void setAhead(double distance) {
+        double relativeDistance = (distance * direction);
+        super.setAhead(relativeDistance);
+        // If distance is negative, reverse our direction
+        if (distance < 0) {
+            direction *= -1;
+        }
+    }
+    
+    public void setBack(double distance) {
+        double relativeDistance = (distance * direction);
+        super.setBack(relativeDistance);
+        // If distance is positive, reverse our direction
+        if (distance > 0) {
+            direction *= -1;
+        }
+    }
+    
+    public void setTurnLeftRadiansOptimal(double angle) {
+        double turn = normalizeRelativeAngleRadians(angle);
+        if (Math.abs(turn) > HALF_PI) {
+            reverseDirection();
+            if (turn < 0) {
+                turn = (HALF_PI + (turn % HALF_PI));
+            } else if (turn > 0) {
+                turn = -(HALF_PI - (turn % HALF_PI));
+            }
+        }
+        setTurnLeftRadians(turn);
+    }
+    
+    public void setTurnRightRadiansOptimal(double angle) {
+        double turn = normalizeRelativeAngleRadians(angle);
+        if (Math.abs(turn) > HALF_PI) {
+            reverseDirection();
+            if (turn < 0) {
+                turn = (HALF_PI + (turn % HALF_PI));
+            } else if (turn > 0) {
+                turn = -(HALF_PI - (turn % HALF_PI));
+            }
+        }
+        setTurnRightRadians(turn);
+    }
+    
+    public double calculateBearingToXYRadians(double sourceX, double sourceY,
+            double sourceHeading,
+            double targetX, double targetY) {
+        return normalizeRelativeAngleRadians(
+                Math.atan2((targetX - sourceX), (targetY - sourceY)) -
+                sourceHeading);
+    }
+    
+    public double normalizeAbsoluteAngleRadians(double angle) {
+        if (angle < 0) {
+            return (DOUBLE_PI + (angle % DOUBLE_PI));
+        } else {
+            return (angle % DOUBLE_PI);
+        }
+    }
+    
+    public static double normalizeRelativeAngleRadians(double angle) {
+        double trimmedAngle = (angle % DOUBLE_PI);
+        if (trimmedAngle > Math.PI) {
+            return -(Math.PI - (trimmedAngle % Math.PI));
+        } else if (trimmedAngle < -Math.PI) {
+            return (Math.PI + (trimmedAngle % Math.PI));
+        } else {
+            return trimmedAngle;
+        }
+    }
+    
+    
+    
 	
 	public double calculatePower(double distance) {
 		final double maxPower = 7;
